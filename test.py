341b69219fc68f5877bf32da88eac92914dbca96
Fredrik Jägare Lilja <fredrik@jagare-lilja.se>
2018-05-02 08:45:51 +0200

    Pasting in the old code.

--- test.py
+++ test.py
@@ -58,3 +58,48 @@ if __name__ == '__main__':
         __set_mode('armed')
     else:
         __set_mode('schedule')
+        
+
+
+
+# import json
+# import logging
+# 
+# import sectoralarm
+# import config
+# 
+# 
+# 
+# def __get_status():
+#     SECTORSTATUS = sectoralarm.SectorStatus(config)
+# 
+#     result = SECTORSTATUS.status()
+# 
+#     print json.dumps(result)
+#     return result['ArmedStatus']
+# 
+# def __set_mode(mode_name):
+# 
+#     from pyarlo import PyArlo
+#     arlo  = PyArlo('fredrik@jagare-lilja.se', '3rNQFmBLNBrK')
+# 
+#     base = arlo.base_stations[0]
+#     print "Is mode " + base.mode
+# 
+#     logging.info('I told you so')
+#     if base.mode != mode_name:
+#         print "Setting mode " + mode_name
+#         base.mode = mode_name
+#         base.update()
+#     else:
+#         print "Mode not set"
+# 
+# if __name__ == '__main__':
+#     logging.basicConfig(filename='example.log',level=logging.INFO)
+# 
+#     status = __get_status()
+#     print "Alarm status is " + status
+#     if status == 'armed':
+#         __set_mode('armed')
+#     else:
+#         __set_mode('schedule')
