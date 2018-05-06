00e9f9dc2ebe8b8c99b789c7cb460c5d5808e2d2
Fredrik Jägare Lilja <Fredrik@fjl-mbp-02.jagare.local>
2018-05-06 15:25:35 +0200

    Adding parsing of command line args.

--- annex_activate.py
+++ annex_activate.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
 
-import logging
+import sys
+import getopt
 import configparser
+import logging
 
 import sectoralarm
+
+
+
+
+def __printMessage():
+    print('Usage: annex_activate [-f]')
+    print('-f force arming of annex')
+    print('-h this message')
+
+def __optionalArm(activator, status, logging):
+    if (status['AlarmStatus'] == 'armed') and (status['StatusAnnex'] == 'disarmed'):
+        if activator.get_last_event() == 'armed':
+            logging.info('Arming annex')
+            activator.arm_annex()
 
 class AnnexActivator:
     
     def __init__(self):
+
         config = configparser.ConfigParser()
         config.read('config.cfg')
         numeric_level = getattr(logging, config.get('Logging', 'level').upper(), None)
@@ -42,9 +59,16 @@ if __name__ == '__main__':
     status = activator.get_status()
     logging.debug("Alarm status is " + status['AlarmStatus'])
     logging.debug("Annex status is " + status['StatusAnnex'])
-
-    if (status['AlarmStatus'] == 'armed') and (status['StatusAnnex'] == 'disarmed'):
-        if activator.get_last_event()== 'armed':
-            logging.info('Arming annex')
+    arguments= sys.argv[1:]
+    try:
+        opts, args= getopt.getopt(sys.argv[1:], 'hf', '')
+    except getopt.GetoptError:
+        __printMessage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            __printMessage()
+        elif opt == '-f':
             activator.arm_annex()
-            
+        else:
+            __optionalArm(activator, status, logging)
