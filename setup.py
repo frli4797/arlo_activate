507df27d1529b818f8fe181764b835b02b00e8a2
Fredrik Jägare Lilja <Fredrik@fjl-mbp-02.jagare.local>
2018-05-09 11:16:38 +0200

    Introducing e-mail notifications.

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+'''
+from distutils.core import setup
+
+
+setup(
+    name='arloactivate',
+    version='1.0.2.1',
+    packages=['sectoralarm'],
+    url='https://github.com/frli4797/sectoralarm',
+    license='MIT License',
+    author='Fredrik Jägare Lilja',
+    author_email='noreply@dummmy.gha',
+    description='A simple library for Sector Alarm API.',
+    install_requires=[
+          'requests',
+    ],
+)
+'''
