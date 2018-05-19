dbade87bdc0473dce66780851021d59ed2a044c9
Fredrik Jägare Lilja <Fredrik@fjl-mbp-02.jagare.local>
2018-05-19 14:38:02 +0200

    Adding some unit tests.

---
+++ test/ArloActivatorTest.py
@@ -0,0 +1,72 @@
+import arlo_activate
+import unittest
+import os
+import tempfile
+import json
+from unittest.mock import MagicMock
+  
+
+class ArloActivatorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.activator = arlo_activate.ArloActivator()
+    
+   
+    def __write_disarmed(self):
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        status = '{"StatusAnnex": "disarmed", "AnnexAvailable": null, "AlarmStatus": "disarmed"}'
+        with open(status_file, 'w') as result_file:
+            result_file.truncate()
+            result_file.write(status)
+            result_file.close()
+    
+    def __write_armed(self):    
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        status = '{"StatusAnnex": "disarmed", "AnnexAvailable": null, "AlarmStatus": "armed"}' 
+        with open(status_file, 'w') as result_file:
+            result_file.truncate()
+            result_file.write(status)
+            result_file.close()
+            
+    def __read_tmp(self):
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        with open(status_file, 'r') as result_file:
+            result_json = json.load(result_file) 
+            result_file.close()
+        return result_json
+    
+    def test_status_should_be_schedule(self):
+        self.activator.set_arlo_mode('schedule')
+        mode = self.activator.get_arlo_mode()
+    
+        self.assertEqual(mode, 'schedule', 'Mode should be schedule.')
+   
+    @unittest.skip("Skipping armed")
+    def test_status_should_be_armed(self):
+        self.activator.set_arlo_mode('armed')
+        mode = self.activator.get_arlo_mode()
+    
+        self.assertEqual(mode, 'armed', 'Mode should be armed.')
+    
+    def test_disarmed_armed_should_be_dirty(self):
+        self.activator.alarm.status = MagicMock(return_value = {"StatusAnnex": "disarmed", "AnnexAvailable": None, "AlarmStatus": "armed"})
+        self.__write_disarmed()
+        alarm_status = self.activator.get_alarm_status()
+        
+        self.assertEqual(alarm_status.dirty, True, 'Should be dirty')
+        self.assertEqual(alarm_status.state['AlarmStatus'], 'armed', 'Should be armed')
+        stored = self.__read_tmp()
+        self.assertEqual(alarm_status.state['AlarmStatus'], stored['AlarmStatus'], 'Stored state is wrong, should be armed.')
+    
+    def test_disarmed_armed_should_be_not_dirty(self):
+        self.activator.alarm.status = MagicMock(return_value = {"StatusAnnex": "disarmed", "AnnexAvailable": None, "AlarmStatus": "disarmed"})
+        self.__write_disarmed()
+        alarm_status = self.activator.get_alarm_status()
+        
+        self.assertEqual(alarm_status.dirty, False, 'Should be dirty')
+        self.assertEqual(alarm_status.state['AlarmStatus'], 'disarmed', 'Should be disarmed')
+        stored = self.__read_tmp()
+        self.assertEqual(alarm_status.state['AlarmStatus'], stored['AlarmStatus'], 'Stored state is wrong, should be disarmed.')
+    
+if __name__ == '__main__':
+    unittest.main()
