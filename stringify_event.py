3e37ba0debb0ee5439030b53a3dbdb5e69c16d87
frli4797 <fredrik@jagare-lilja.se>
2018-10-12 08:35:30 +0200

    Stringify event handler.

---
+++ stringify_event.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import logging
+import tempfile
+import os
+import configparser
+import sys
+
+import sectoralarm
+from requests import HTTPError
+
+
+class alarm_state:
+    """
+    Alarm state class.
+    Consists of a dirty bit and the actual state.
+    """
+
+    dirty = False
+    state = ""
+
+    def __init__(self, dirty, state):
+        self.dirty = dirty
+        self.state = state
+
+
+class StringifyActivator:
+    """
+    Container class for Arlo Activator.
+    """
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.cfg')
+        numeric_level = getattr(logging, config.get('Logging', 'level').upper(), None)
+        log_location = config.get('Logging', 'log_location')
+        logfile = os.path.join(log_location, 'arlo_activate.log')
+        logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+                            filename=logfile, level=numeric_level)
+        self.alarm_email = config.get('Alarm', 'email')
+        self.alarm_password = config.get('Alarm', 'password')
+        self.alarm_siteId = config.get('Alarm', 'siteId')
+
+        self.activation_url = config.get('Stringify', 'activate_url')
+        self.deactivation_url = config.get('Stringify', 'deactivate_url')
+
+        self.alarm = sectoralarm.connect(self.alarm_email, self.alarm_password, self.alarm_siteId)
+
+    def get_alarm_status(self):
+        """
+        Returns alarm state as an alarm_state.
+        """
+
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        logging.debug('Tempfile ' + status_file)
+        current_status = self.alarm.status()
+        logging.debug(json.dumps(current_status))
+
+        if current_status['AlarmStatus'] not in {'armed', 'disarmed'} or \
+                current_status['StatusAnnex'] not in {'armed', 'disarmed'}:
+            logging.error("Alarm status is " + current_status['AlarmStatus'])
+            logging.error("Annex status is " + current_status['StatusAnnex'])
+            raise ValueError('Returned status from alarm is not valid.')
+
+        saved_status = {'AlarmStatus': 'notset'}
+
+        if os.path.isfile(status_file):
+            with open(status_file, 'r+') as result_file:
+                saved_status = json.load(result_file)
+                result_file.seek(0)
+                result_file.truncate()
+                json.dump(current_status, result_file)
+                result_file.close()
+                logging.debug('New stored status: ' + saved_status['AlarmStatus'])
+        else:
+            with open(status_file, 'w') as result_file:
+                json.dump(current_status, result_file)
+                result_file.close()
+
+        logging.debug('Stored status: ' + saved_status['AlarmStatus'])
+        logging.debug('Current status: ' + current_status['AlarmStatus'])
+
+        if current_status['AlarmStatus'] == saved_status['AlarmStatus']:
+            logging.debug('Setting to not dirty.')
+            return alarm_state(False, current_status)
+        else:
+            logging.debug('Setting to dirty.')
+            return alarm_state(True, current_status)
+
+    def send_stringify(self, activation):
+        """
+        Sends event to Stringify
+        """
+
+        try:
+            if activation:
+                url = self.activation_url
+            else:
+                url = self.deactivation_url
+
+            logging.info("Sending request to stringify " + url)
+            result = requests.get(url)
+            result.raise_for_status()
+
+        except HTTPError:
+            logging.error('Got type error. Doing nothing. ', exc_info=True)
+
+
+if __name__ == '__main__':
+    event_sender = StringifyActivator()
+    alarm_status = 'none'
+
+    try:
+        alarm_status = StringifyActivator.get_alarm_status()
+        logging.debug("Alarm status is " + alarm_status.state['AlarmStatus'])
+    except:
+        logging.error("Could not read status.", exc_info=True)
+        sys.exit(1)
+
+    if not alarm_status.dirty:
+        logging.debug('Not dirty. Doing nothing.')
+    else:
+        if alarm_status.state['AlarmStatus'] == 'armed':
+            event_sender.send_stringify(True)
+        else:
+            event_sender.send_stringify(False)
---
+++ test/stringify_event_test.py
@@ -0,0 +1,68 @@
+import arlo_activate
+import unittest
+import os
+import tempfile
+import json
+import time
+from unittest.mock import MagicMock
+
+
+class StringifyEventTest(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.activator = arlo_activate.arlo_activator()
+
+    def __write_disarmed(self):
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        status = '{"StatusAnnex": "disarmed", "AnnexAvailable": null, "AlarmStatus": "disarmed"}'
+        with open(status_file, 'w') as result_file:
+            result_file.truncate()
+            result_file.write(status)
+            result_file.close()
+
+    def __write_armed(self):
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        status = '{"StatusAnnex": "disarmed", "AnnexAvailable": null, "AlarmStatus": "armed"}'
+        with open(status_file, 'w') as result_file:
+            result_file.truncate()
+            result_file.write(status)
+            result_file.close()
+
+    def __read_tmp(self):
+        status_file = os.path.join(tempfile.gettempdir(), 'alarm_state')
+        with open(status_file, 'r') as result_file:
+            result_json = json.load(result_file)
+            result_file.close()
+        return result_json
+
+    def test_disarmed_armed_should_be_dirty(self):
+        self.activator.alarm.status = MagicMock(
+            return_value={"StatusAnnex": "disarmed", "AnnexAvailable": None, "AlarmStatus": "armed"})
+        self.__write_disarmed()
+        alarm_status = self.activator.get_alarm_status()
+
+        self.assertEqual(alarm_status.dirty, True, 'Should be dirty')
+        self.assertEqual(alarm_status.state['AlarmStatus'], 'armed', 'Should be armed')
+        stored = self.__read_tmp()
+        self.assertEqual(alarm_status.state['AlarmStatus'], stored['AlarmStatus'],
+                         'Stored state is wrong, should be armed.')
+
+    def test_disarmed_armed_should_be_not_dirty(self):
+        self.activator.alarm.status = MagicMock(
+            return_value={"StatusAnnex": "disarmed", "AnnexAvailable": None, "AlarmStatus": "disarmed"})
+        self.__write_disarmed()
+        alarm_status = self.activator.get_alarm_status()
+
+        self.assertEqual(alarm_status.dirty, False, 'Should be dirty')
+        self.assertEqual(alarm_status.state['AlarmStatus'], 'disarmed', 'Should be disarmed')
+        stored = self.__read_tmp()
+        self.assertEqual(alarm_status.state['AlarmStatus'], stored['AlarmStatus'],
+                         'Stored state is wrong, should be disarmed.')
+
+    def tearDown(self):
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    unittest.main()
